e8900b802cdb0eff6d329d059591a0735259a1c7
tlnguy05 <hoangtuan.vtn3@gmail.com>
2020-08-18 17:37:04 -0400

    update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -20,10 +20,36 @@ subject_test <- fread("./UCI HAR Dataset/test/subject_test.txt")
 
 # Read the feature description
 # Only extract the 2nd column (the ID column is unnecessary)
-features <- fread("./UCI HAR Dataset/features.txt", select = 2)
+features <- fread("./UCI HAR Dataset/features.txt")
 
 # Read the activity labels
 # Only extract the 2nd column (the ID column is unnecessary)
 activity_labels <- fread("./UCI HAR Dataset/activity_labels.txt")
 
 # 1.Merges the training and the test sets to create one data set.
+x_merge <- rbind(x_train, x_test)
+y_merge <- rbind(y_train, y_test)
+subject_merge <- rbind(subject_train, subject_test)
+newdata <- cbind(x_merge, y_merge, subject_merge)
+
+# 2.Extracts only the measurements on the mean and standard deviation for each 
+# measurement.
+# To match metacharacters in R, use a double backslash "\\"
+mean_std_features <- grep(".*mean\\(\\)|std\\(\\)", features$V2)
+x_merge <- x_merge[, ..mean_std_features] #Use ".." to extract columns in data table
+
+# 3. Uses descriptive activity names to name the activities in the data set
+y_merge$V1 <- factor(y_merge$V1, levels = activity_labels$V1, labels = activity_labels$V2)
+
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(x_merge) <- features$V2[mean_std_features]
+colnames(y_merge) <- "activity"
+colnames(subject_merge) <- "subject"
+
+# 5. From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+newdata <- cbind(x_merge, y_merge, subject_merge)
+library(dplyr)
+tidydata <- newdata %>% group_by(activity, subject) %>%
+            summarise_all(funs(mean))
+write.table(tidydata, file = "./tidydata.txt", row.names = FALSE, col.names = TRUE)
